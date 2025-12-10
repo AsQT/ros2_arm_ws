852e8bf24a15c997d4e2af0bac5ef81262859cc0
AsQT <Tranminhquang617@outlook.com.vn>
2025-12-10 15:44:12 +0700

    update git tracking

--- build/robot_moveit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/minhquang/ros2_arm_ws/src/robot_moveit
-# Build directory: /home/minhquang/ros2_arm_ws/build/robot_moveit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/robot_moveit/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/minhquang/ros2_arm_ws/install/robot/share/robot/package.sh"
